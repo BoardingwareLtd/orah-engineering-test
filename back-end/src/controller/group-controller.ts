7423dafe818f9e87598a01a743d8828400b34f51
Manav.Garg <Manav.Garg@ril.com>
2023-05-11 01:39:24 +0530

    modified runGroupFilters to promise approach to remove a bug

--- back-end/src/controller/group-controller.ts
+++ back-end/src/controller/group-controller.ts
@@ -1,39 +1,39 @@
-import { NextFunction, Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { GroupStudent } from "../entity/group-student.entity";
-import { Group } from "../entity/group.entity";
-import { Roll } from "../entity/roll.entity";
-import { StudentRollState } from "../entity/student-roll-state.entity";
-import { CreateGroupStudentInput } from "../interface/group-student.interface";
-import { CreateGroupInput, UpdateGroupInput } from "../interface/group.interface";
+import { NextFunction, Request, Response } from "express"
+import { getRepository } from "typeorm"
+import { GroupStudent } from "../entity/group-student.entity"
+import { Group } from "../entity/group.entity"
+import { Roll } from "../entity/roll.entity"
+import { StudentRollState } from "../entity/student-roll-state.entity"
+import { CreateGroupStudentInput } from "../interface/group-student.interface"
+import { CreateGroupInput, UpdateGroupInput } from "../interface/group.interface"
 
 export class GroupController {
-  private groupRepository = getRepository(Group);
+  private groupRepository = getRepository(Group)
   private studentGroupRepository = getRepository(GroupStudent)
   private studentRollStateRepository = getRepository(StudentRollState)
   private rollRepository = getRepository(Roll)
 
   async allGroups(request: Request, response: Response, next: NextFunction) {
     try {
-      const groups = await this.groupRepository.find();
-      return groups;
+      const groups = await this.groupRepository.find()
+      return groups
     } catch (error) {
-      return next(error);
+      return next(error)
     }
   }
 
   async getgroupstudents(request: Request, response: Response, next: NextFunction) {
     try {
-      return await this.studentGroupRepository.find();
+      return await this.studentGroupRepository.find()
       // return groups;
     } catch (error) {
-      return next(error);
+      return next(error)
     }
   }
 
   async createGroup(request: Request, response: Response, next: NextFunction) {
     try {
-      const { body: params } = request;
+      const { body: params } = request
 
       const createGroupInput: CreateGroupInput = {
         name: params.name,
@@ -41,24 +41,24 @@ export class GroupController {
         roll_states: params.roll_states,
         incidents: params.incidents,
         ltmt: params.ltmt,
-      };
+      }
 
-      const group = new Group();
-      group.prepareToCreate(createGroupInput);
+      const group = new Group()
+      group.prepareToCreate(createGroupInput)
 
-      return await this.groupRepository.save(group);
+      return await this.groupRepository.save(group)
     } catch (error) {
-      return next(error);
+      return next(error)
     }
   }
 
   async updateGroup(request: Request, response: Response, next: NextFunction) {
     try {
-      const { body: params } = request;
-      const group = await this.groupRepository.findOne(params.id);
+      const { body: params } = request
+      const group = await this.groupRepository.findOne(params.id)
 
       if (!group) {
-        throw new Error("Group not found");
+        throw new Error("Group not found")
       }
 
       const updateRollInput: UpdateGroupInput = {
@@ -68,27 +68,27 @@ export class GroupController {
         roll_states: params.roll_states,
         incidents: params.incidents,
         ltmt: params.ltmt,
-      };
+      }
 
-      group.prepareToUpdate(updateRollInput);
-      return await this.groupRepository.save(group);
+      group.prepareToUpdate(updateRollInput)
+      return await this.groupRepository.save(group)
     } catch (error) {
-      return next(error);
+      return next(error)
     }
   }
 
   async removeGroup(request: Request, response: Response, next: NextFunction) {
     try {
-      const { body: params } = request;
-      const group = await this.groupRepository.findOne(params.id);
+      const { body: params } = request
+      const group = await this.groupRepository.findOne(params.id)
 
       if (!group) {
-        throw new Error("Group not found");
+        throw new Error("Group not found")
       }
 
-      return await this.groupRepository.remove(group);
+      return await this.groupRepository.remove(group)
     } catch (error) {
-      return next(error);
+      return next(error)
     }
   }
 
@@ -96,75 +96,79 @@ export class GroupController {
     try {
       // Task 1:
       // Return the list of Students that are in a Group
-
     } catch (error) {
-      return next(error);
+      return next(error)
     }
   }
 
   async runGroupFilters(request: Request, response: Response, next: NextFunction) {
     try {
       // 1. Clear out the groups (delete all the students from the groups)
-      const groupsstudents = await this.studentGroupRepository.find();
-      groupsstudents.forEach(async(groupsstudent: GroupStudent)=>{
-        await this.studentGroupRepository.remove(groupsstudent);
+      const groupsstudents = await this.studentGroupRepository.find()
+      groupsstudents.forEach(async (groupsstudent: GroupStudent) => {
+        await this.studentGroupRepository.remove(groupsstudent)
       })
+
       // 2. For each group, query the student rolls to see which students match the filter for the group
-      const groups = await this.groupRepository.find();
-      const studentrolemapping = await this.studentRollStateRepository.find()
-      const rolls = await this.rollRepository.find()
-      groups.map(async (group)=>{
-        let studentgroupmapping = [];
-        const {id,number_of_weeks,roll_states,incidents,ltmt} = group;
-        const fromDate = new Date();
-        fromDate.setDate(fromDate.getDate() - number_of_weeks * 7);
-        const filtered_rolls = rolls.filter((roll)=>{
-          return roll.completed_at>=fromDate
-        })
-
-        filtered_rolls.map((roll)=>{
-          const studentlist_in_roll = studentrolemapping.filter((stdrole)=>{
-            return stdrole.roll_id==roll.id && group.roll_states.split(",").includes(stdrole.state)
+      new Promise(async (resolve, reject) => {
+        try {
+          const groups = await this.groupRepository.find()
+          const studentrolemapping = await this.studentRollStateRepository.find()
+          const rolls = await this.rollRepository.find()
+          const promises = groups.map(async (group) => {
+            let studentgroupmapping = this.get_student_group_mapping(group, rolls, studentrolemapping)
+            // 3. Add the list of students that match the filter to the group
+            await this.pushstudentgroupmapping_to_db(studentgroupmapping, group)
           })
-          studentgroupmapping = [...studentgroupmapping,...studentlist_in_roll]
-        })
-        await this.pushstudentgroupmapping_to_db(studentgroupmapping, group);
 
+          await Promise.all(promises)
+        } catch (error) {
+          reject(error)
+        }
       })
-
-      return {
-        "response": "Run group filters successful",
-        "code":200
-      }
-
+      return await this.studentGroupRepository.find()
     } catch (error) {
-      return next(error);
+      return next(error)
     }
   }
 
+  private get_student_group_mapping(group: Group, rolls: Roll[], studentrolemapping: StudentRollState[]) {
+    let studentgroupmapping = []
+    const { id, number_of_weeks, roll_states, incidents, ltmt } = group
+    const fromDate = new Date()
+    fromDate.setDate(fromDate.getDate() - number_of_weeks * 7)
+    const filtered_rolls = rolls.filter((roll) => {
+      return roll.completed_at >= fromDate
+    })
+
+    filtered_rolls.map((roll) => {
+      const studentlist_in_roll = studentrolemapping.filter((stdrole) => {
+        return stdrole.roll_id == roll.id && group.roll_states.split(",").includes(stdrole.state)
+      })
+      studentgroupmapping = [...studentgroupmapping, ...studentlist_in_roll]
+    })
+    return studentgroupmapping
+  }
 
   private async pushstudentgroupmapping_to_db(studentgroupmapping: any[], group: Group) {
-    const studentCounts = {};
-    // 3. Add the list of students that match the filter to the group
+    const studentCounts = {}
     for (const stdgrpmap of studentgroupmapping) {
-      const studentId = stdgrpmap.student_id;
-      studentCounts[studentId] = (studentCounts[studentId] || 0) + 1;
+      const studentId = stdgrpmap.student_id
+      studentCounts[studentId] = (studentCounts[studentId] || 0) + 1
     }
     Object.entries(studentCounts).forEach(async ([key, value]) => {
-      await this.pushtoStudentGroupHelper(Number(key), group.id, Number(value));
-    });
+      await this.pushtoStudentGroupHelper(Number(key), group.id, Number(value))
+    })
   }
 
-  private async pushtoStudentGroupHelper(student_id: number, group_id: number, count: number){
-    const createGroupStudentInput: CreateGroupStudentInput= {
+  private async pushtoStudentGroupHelper(student_id: number, group_id: number, count: number) {
+    const createGroupStudentInput: CreateGroupStudentInput = {
       student_id: student_id,
       group_id: group_id,
-      incident_count: count
+      incident_count: count,
     }
     const groupstudent = new GroupStudent()
     groupstudent.prepareToCreate(createGroupStudentInput)
     await this.studentGroupRepository.save(groupstudent)
+  }
 }
-
-}
-
